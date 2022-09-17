eb690f8a3ed1190996aa9dbe9c85d02d001a23a3
Siia-sea <anastasiaakcelik@gmail.com>
2022-09-17 16:11:41 +0300

    last change

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ a = int(input('a:'))
 b = int(input('b:'))
 c = int(input('c:'))
 
-num = (a >= 10 and b >= 10 and c >= 10)
-print(a%3==0)
-print(b%3==0)
+if a >= 10 and b >= 10 and c >= 10:
+    if a % 3 == 0 and b % 3 == 0:
+        print('YES')
+    else:
+        print('NO')
